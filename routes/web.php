4686daa80de4bbcf7215c4bbce40f46d1bbba957
Sumit <dipankarsumit151298@gmail.com>
2025-01-10 23:32:57 +0530

    Initial Commit

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+$router->group([
+    'prefix' => 'api'
+], function ($router) {
+    $router->post('login', 'AuthController@login');
+    $router->post('logout', 'AuthController@logout');
+    $router->post('refresh', 'AuthController@refresh');
+    $router->post('user-profile', 'AuthController@me');
+});
